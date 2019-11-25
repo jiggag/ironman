fac851a31a8cebc98008be015863abafaa59ed20
jiggag <jiggag90@gmail.com>
2019-11-25 22:49:36 +0900

    fix: 로딩이 완료된 후 목록 표시하도록 수정 및 인트로 화면으로 이동

--- src/pages/ListNote/Container.tsx
+++ src/pages/ListNote/Container.tsx
@@ -3,7 +3,7 @@ import Spinner from 'react-native-loading-spinner-overlay';
 import { SafeAreaView } from 'react-native';
 import styles from './styles';
 import Presenter from './Presenter';
-import { Actions } from 'react-native-router-flux';
+import { Actions, ActionConst } from 'react-native-router-flux';
 import { RESTful } from '../../utils';
 
 const Container = ({ update = false }) => {
@@ -46,11 +46,11 @@ const Container = ({ update = false }) => {
 
   const onActionToCreate = () => Actions.createNote();
   const onPress = id => Actions.detailNote({ id });
-  const onPressBack = () => Actions.pop();
+  const onPressBack = () => Actions.intro({ type: ActionConst.RESET });
 
   return (
     <SafeAreaView style={styles.safeAreaView}>
-      <Presenter list={list} onActionToCreate={onActionToCreate} onPress={onPress} onPressBack={onPressBack} />
+      <Presenter isLoading={isLoading} list={list} onActionToCreate={onActionToCreate} onPress={onPress} onPressBack={onPressBack} />
       <Spinner visible={isLoading} />
     </SafeAreaView>
   );
--- src/pages/ListNote/Presenter.tsx
+++ src/pages/ListNote/Presenter.tsx
@@ -6,30 +6,36 @@ import styles from './styles';
 import {Header, BigButton} from '../../components';
 import Constant from '../../utils/constants';
 
-const Presenter = ({ list, onActionToCreate, onPress, onPressBack }) => (
+const Presenter = ({ isLoading, list, onActionToCreate, onPress, onPressBack }) => (
   <>
     <Header onPress={onPressBack} />
     <ScrollView style={styles.container}>
       {
-        list.length > 0 ? (
-          list.map(item => {
-            const {id, date, title} = item;
-            return (
-              <View paddingH-20 marginB-10 marginT-10={id === 1} key={id}>
-                <TouchableOpacity activeOpacity={0.8} onPress={() => onPress(id)}>
-                  <View paddingV-10 paddingH-15 style={styles.noteCard}>
-                    <Text style={styles.dateText}>{moment(date).format('YYYY.MM.DD')}</Text>
-                    <Text style={styles.titleText}>{title}</Text>
+          !isLoading ? (
+              list.length > 0 ? (
+              list.map(item => {
+                const {id, date, title} = item;
+                return (
+                  <View paddingH-20 marginB-10 marginT-10={id === 1} key={id}>
+                    <TouchableOpacity activeOpacity={0.8} onPress={() => onPress(id)}>
+                      <View paddingV-10 paddingH-15 style={styles.noteCard}>
+                        <Text style={styles.dateText}>{moment(date).format('YYYY.MM.DD')}</Text>
+                        <Text style={styles.titleText}>{title}</Text>
+                      </View>
+                    </TouchableOpacity>
                   </View>
-                </TouchableOpacity>
+                );
+              })
+            ) : (
+              <View style={styles.emptyCard}>
+                <Text>없음</Text>
               </View>
-            );
-          })
-        ) : (
-          <View style={styles.emptyCard}>
-            <Text>없음</Text>
-          </View>
-        )
+            )
+          ) : (
+            <View style={styles.emptyCard}>
+                <Text>로딩중...</Text>
+            </View>
+          )
       }
     </ScrollView>
     <BigButton
