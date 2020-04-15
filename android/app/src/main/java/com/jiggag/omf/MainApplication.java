6d8084e21a5e648d785f3ae69ac4bf5030d17972
jiggag <jiggag90@gmail.com>
2020-04-15 22:40:21 +0900

    fix: 안드로이드 패키지 import

--- android/app/src/main/java/com/jiggag/omf/MainApplication.java
+++ android/app/src/main/java/com/jiggag/omf/MainApplication.java
@@ -8,6 +8,7 @@ import android.util.Base64;
 import android.content.Context;
 import android.util.Log;
 
+import com.facebook.react.BuildConfig;
 import com.facebook.react.PackageList;
 import com.google.android.play.core.missingsplits.MissingSplitsManagerFactory;
 import com.facebook.react.ReactApplication;
