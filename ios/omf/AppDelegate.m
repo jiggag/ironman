1371cc037a43f53ce1ad3bdf82e37f117fc41486
jiggag <jiggag90@gmail.com>
2020-07-12 21:26:43 +0900

    fix: iOS flipper 원복

--- ios/omf/AppDelegate.m
+++ ios/omf/AppDelegate.m
@@ -12,23 +12,23 @@
 // or just fetch the whole config
 //NSDictionary *config = [ReactNativeConfig env];
 
-// #if DEBUG
-// #import <FlipperKit/FlipperClient.h>
-// #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
-// #import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
-// #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
-// #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
-// #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
-// static void InitializeFlipper(UIApplication *application) {
-//   FlipperClient *client = [FlipperClient sharedClient];
-//   SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
-//   [client addPlugin:[[FlipperKitLayoutPlugin alloc] initWithRootNode:application withDescriptorMapper:layoutDescriptorMapper]];
-//   [client addPlugin:[[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
-//   [client addPlugin:[FlipperKitReactPlugin new]];
-//   [client addPlugin:[[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
-//   [client start];
-// }
-// #endif
+#if DEBUG
+#import <FlipperKit/FlipperClient.h>
+#import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
+#import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
+#import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
+#import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
+#import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+static void InitializeFlipper(UIApplication *application) {
+  FlipperClient *client = [FlipperClient sharedClient];
+  SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
+  [client addPlugin:[[FlipperKitLayoutPlugin alloc] initWithRootNode:application withDescriptorMapper:layoutDescriptorMapper]];
+  [client addPlugin:[[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
+  [client addPlugin:[FlipperKitReactPlugin new]];
+  [client addPlugin:[[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
+  [client start];
+}
+#endif
 
 @implementation AppDelegate
 
@@ -39,9 +39,9 @@
     [FIRApp configure];
   }
 
-// #if DEBUG
-//   InitializeFlipper(application);
-// #endif
+#if DEBUG
+  InitializeFlipper(application);
+#endif
 
   [ChannelIO initialize:application];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
