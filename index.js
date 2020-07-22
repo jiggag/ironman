097876cff9e1e6df073caf740bfcae83e63d8b5e
jiggag <jiggag90@gmail.com>
2020-07-22 23:47:28 +0900

    feat: #85 react-native v0.63.1 android

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ import { init as SentryInit } from '@sentry/react-native';
 import Config from 'react-native-config';
 import App from './src/App';
 
-if (__DEV__) {
-  require('react-native').unstable_enableLogBox();
-}
-
 SentryInit({
   dsn: Config.SENTRY_DSN,
 });
