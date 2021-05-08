5a259aed82483f7939a4ecc365dc8271f5769ca4
jiggag <jiggag90@gmail.com>
2021-05-08 20:37:02 +0900

    DEV-71 다크모드 대비 색상 정리

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,5 @@
+import { Platform } from 'react-native';
+
 const Constant = {
   MAIN_COLOR: '#209EFF',
   SUB_COLOR: '#eee',
@@ -11,3 +13,56 @@ const Constant = {
 };
 
 export default Constant;
+
+export const Color = {
+  white: '#F2F2F2',
+  black: '#333333',
+  blue: '#2F80ED',
+  red: '#EB5757',
+  gray: '#BDBDBD',
+};
+
+export const Theme = {
+  light: {
+    base: Color.blue,
+    text: Color.white,
+    main: Color.red,
+    sub: Color.red,
+    option: Color.gray,
+  },
+  dark: {
+    base: Color.black,
+    text: Color.white,
+    main: Color.blue,
+    sub: Color.red,
+    option: Color.gray,
+  },
+};
+
+export const FontSize = {
+  title: 36,
+  button: 18,
+  normal: 14,
+  small: 10,
+};
+
+export const FontStyle = {
+  normal: 'normal',
+  italic: 'italic',
+} as const;
+
+export const FontWeight = {
+  bold: 'bold',
+  normal: 'normal',
+  light: Platform.select({
+    ios: '200',
+    android: 'normal',
+  }),
+} as const;
+
+export const DefaultFontStyle = {
+  fontSize: FontSize.normal,
+  fontStyle: FontStyle.normal,
+  fontWeight: FontWeight.normal,
+  includeFontPadding: false,
+};
